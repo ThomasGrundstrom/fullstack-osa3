8fbc7621feb8362536d0fc18fe96fc3c35f4e826
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-05-27 18:29:03 +0300

    ESLint-muutoksia

--- index.js
+++ index.js
@@ -13,44 +13,21 @@ const customFormat = function (tokens, req, res) {
             tokens.res(req, res, 'content-length'), '-',
             tokens['response-time'](req, res), 'ms',
             JSON.stringify(req.body)
-          ].join(' ')
+        ].join(' ')
     }
     return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms'
     ].join(' ')
-  }
+}
 
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(customFormat))
 app.use(cors())
-
-let persons = [
-  { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
-  },
-  { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-  },
-  { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -84,12 +61,12 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id).then(result => {
+    Person.findByIdAndDelete(request.params.id).then(() => {
         response.status(204).end()
     })
-    .catch(error => {
-        next(error)
-    })
+        .catch(error => {
+            next(error)
+        })
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -109,9 +86,9 @@ app.post('/api/persons', (request, response, next) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
-    .catch(error => {
-        next(error)
-    })
+        .catch(error => {
+            next(error)
+        })
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -130,7 +107,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
@@ -141,5 +118,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -5,7 +5,7 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 console.log(`connecting to ${url}`)
-mongoose.connect(url).then(result => {
+mongoose.connect(url).then(() => {
     console.log('connected to MongoDB')
 }).catch((error) => {
     console.log(`error connecting to MongoDB: ${error.message}`)
